9d7b01c804e6958d979b421d2f7e53784f5cddcf
Gonzalo Mendoza <yogona524@gmail.com>
2016-11-08 23:24:01 -0300

    Salvando los cambios, version estable

--- src/main/java/com/core/meka/SOMController.java
+++ src/main/java/com/core/meka/SOMController.java
@@ -9,14 +9,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -25,15 +32,19 @@ import javafx.scene.control.TitledPane;
  */
 public class SOMController implements Initializable {
 
+    @FXML private AnchorPane base;
+    
     @FXML private TitledPane t1;
     @FXML private Accordion ac;
-    @FXML private Button cargar_pesos_btn;
     @FXML private CheckBox pesos_check;
     @FXML private ComboBox funcion_vecindad_combo;
     @FXML private TextField entradas_text;
     @FXML private TextField epocas_text;
     @FXML private TextField neuronas_text;
+    @FXML private TextArea patrones_train_text;
+    @FXML private TextArea pesos_text;
     @FXML private Button entrenar_btn;
+    @FXML private Label pesos_label;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,11 +56,39 @@ public class SOMController implements Initializable {
 
     private void initConfig() {
         this.ac.setExpandedPane(t1);
-//        this.cargar_pesos_btn.setVisible(false);
+        pesos_label.setVisible(false);
+        pesos_text.setVisible(false);
+        pesos_check.setSelected(true);
     }
 
     private void initButtons() {
-        
+        entrenar_btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (entradas_text.getText().isEmpty()) {
+                    return;
+                }
+                if(epocas_text.getText().isEmpty()){
+                    return;
+                }
+                if (neuronas_text.getText().isEmpty()) {
+                    return;
+                }
+                if (patrones_train_text.getText().isEmpty()) {
+                    return;
+                }
+                if (funcion_vecindad_combo.getSelectionModel().getSelectedIndex() < 0) {
+                    return;
+                }
+                //Valores correctos
+                String[] patrones = patrones_train_text.getText().replaceAll("\\s+","").replaceAll("\\n", "").split(";");
+                if (patrones.length == 0) {
+                    return;
+                }
+                
+                
+            }
+        });
     }
 
     private void initListeners() {
@@ -84,10 +123,20 @@ public class SOMController implements Initializable {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
                 if (!newValue) {
-                    cargar_pesos_btn.setVisible(true);
+                    pesos_label.setVisible(true);
+                    pesos_text.setVisible(true);
                 }
                 else{
-                    cargar_pesos_btn.setVisible(false);
+                    pesos_label.setVisible(false);
+                    pesos_text.setVisible(false);
+                }
+            }
+        });
+        base.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode().equals(KeyCode.ESCAPE)) {
+                    Util.closeWindow(ac);
                 }
             }
         });
